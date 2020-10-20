4f695aa8420b8ce9284a53cccf1ead8f313d5878
Nixon Enraght-Moony <nixon.emoony@gmail.com>
2020-10-20 13:43:19 +0100

    ____                       __,-~~/~    `---.                     _/_,---(      ,    )                 __ /        <    /   )  \___                ====------------------===;;;==                    \/  ~"~"~"~"~"~\~"~)~",1/                    (_ (   \  (     >    \)                     \_( _ <         >_>'                        ~ `-i' ::>|--"                            I;|.|.|                           <|i::|i|>                            |[::|.|                             ||: | _________________________NINJA SYNTAX__________________

--- ninja-syntax/reset-py.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-set -euo pipefail
-
-rm ninja_syntax*.py
-curl -O https://raw.githubusercontent.com/ninja-build/ninja/master/misc/ninja_syntax.py
-curl -O https://raw.githubusercontent.com/ninja-build/ninja/master/misc/ninja_syntax_test.py
