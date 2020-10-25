0d20f831042c2902ff046ea225c3f0c3c15b74da
Nixon Enraght-Moony <nixon.emoony@gmail.com>
2020-10-25 15:15:52 +0000

    Fix redactions

--- src/config.rs
+++ src/config.rs
@@ -99,21 +99,27 @@ mod tests {
 
     #[test]
     fn hydrate_dummy() {
+        // I don't know why I need the anotation
+        let redacter = |mut val, _: insta::internals::ContentPath| {
+            // TODO: Dont do insta crimes.
+            // This is in #[doc(Hidden)] internals.
+            while let insta::internals::Content::Some(some) = val {
+                val = *some;
+            }
+
+            if let insta::internals::Content::String(s) = val {
+                s.replace(dbg!(env!("CARGO_MANIFEST_DIR")), "BASEDIR")
+            } else {
+                "".into()
+            }
+        };
+
         let dir = PathBuf::from(env!("CARGO_MANIFEST_DIR")).join("dummy-book");
         let args = cli::Args { dir, create: false };
         let conf = load(args).unwrap();
         assert_yaml_snapshot!(conf, {
-            ".**.location" => insta::dynamic_redaction(|val, _| {
-                if let insta::internals::Content::Some(some) = val {
-                    if let insta::internals::Content::String(s) = *some {
-                        s.replace(env!("CARGO_MANIFEST_DIR"), "BASEDIR")
-                    } else {
-                        "".into()
-                    }
-                } else {
-                    "".into()
-                }
-            })
+            ".location" => insta::dynamic_redaction(redacter),
+            ".**.location" => insta::dynamic_redaction(redacter),
         });
     }
 }
