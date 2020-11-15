a0baccfcd6370769e03cf60835a1bbb93601eed2
Nixon Enraght-Moony <nixon.emoony@gmail.com>
2020-11-15 12:07:46 +0000

    Implement most of capture_pages

--- src/html_render/content.rs
+++ src/html_render/content.rs
@@ -1,7 +1,8 @@
 use crate::config::{self, Book as CBook};
 use crate::summary;
 use eyre::Result;
-use std::path::PathBuf;
+use std::ffi::OsStr;
+use std::path::{Path, PathBuf};
 
 /// The content in a suitable form.
 
@@ -29,8 +30,8 @@ struct Book {
 pub struct Page {
     /// The html file to render to
     pub output: PathBuf,
-    /// The md input file. None => draft
-    pub input: Option<PathBuf>,
+    /// The md input file.
+    pub input: PathBuf,
     pub toc: pagetoc::PageToc,
     /// The link to the next page
     pub next: Option<String>,
@@ -46,29 +47,39 @@ pub struct Page {
     pub heirachy: Vec<Link>,
 }
 
+pub struct Dirs<'a> {
+    out_dir: &'a Path,
+    base_dir: &'a Path,
+}
+
 impl Content {
-    pub fn new(config: &config::GlobalConf) -> Result<Self> {
+    pub fn new(config: &config::GlobalConf, dirs: Dirs) -> Result<Self> {
         Ok(Self(
-            config.books.iter().map(Book::new).collect::<Result<_>>()?,
+            config
+                .books
+                .iter()
+                .map(|x| Book::new(x, &dirs))
+                .collect::<Result<_>>()?,
         ))
     }
 }
 
 impl Book {
-    pub fn new(book: &config::Book) -> Result<Self> {
+    pub fn new(book: &config::Book, dirs: &Dirs) -> Result<Self> {
         let title = book.summary.title.clone();
-        let pages = Self::capture_pages(book)?;
+        let pages = Self::capture_pages(book, dirs)?;
 
         Ok(Self { title, pages })
     }
 
-    fn capture_pages(book: &config::Book) -> Result<Vec<Page>> {
+    fn capture_pages(book: &config::Book, dirs: &Dirs) -> Result<Vec<Page>> {
         use PageListParts::*;
 
         // We need to hold onto a bungh of stuff as we walk the tree, ands its
         // nicer if thats a list, and we just preserve the tree structure by
         // saying when we go up and down.
 
+        // Create flat list
         let mut pages_parts = Vec::new();
         for i in &book.summary.prefix_chapters {
             pages_parts.push(Chapter(i));
@@ -80,7 +91,60 @@ impl Book {
             pages_parts.push(Chapter(i));
         }
 
-        todo!()
+        let mut out = Vec::<Page>::new();
+
+        let mut heirachy = Vec::new();
+        for i in pages_parts {
+            match i {
+                PageListParts::Chapter(chap) => {
+                    let input = match &chap.location {
+                        // Skip over the drafts, as they only show up in big toc.
+                        None => continue,
+                        Some(l) => l,
+                    }
+                    .clone();
+
+                    let heirachy = heirachy.clone();
+
+                    let output = output_loc(&input, dirs.out_dir, dirs.base_dir)?;
+                    let mut page = Page {
+                        input,
+                        output,
+                        // TODO: I think this is the wrong design, as toc can't
+                        // be determined utill we read the file, which we arn't
+                        // doing here.
+                        toc: Default::default(),
+                        prev: None,
+                        next: None,
+                        heirachy,
+                    };
+                    page.heirachy.push(page.heirachy_element());
+                    out.push(page)
+                }
+                PageListParts::StartSection => {
+                    heirachy.push(out.last().unwrap().heirachy_element());
+                }
+                PageListParts::EndSection => {
+                    heirachy.pop();
+                }
+            }
+        }
+        // We can't have something nice like
+        // for i in out.windows_mut(2) {
+        //     let before = &i[0];
+        //     let after = &i[1];
+        //     before.next = Some(after.url());
+        //     after.prev = Some(before.url());
+        // }
+        // Because GAT's.
+        for i in 0..out.len() - 1 {
+            let before_url = out[i].url();
+            let after_url = out[i + 1].url();
+            out[i].next = Some(after_url);
+            out[i + 1].prev = Some(before_url);
+        }
+
+        Ok(out)
     }
 
     fn capture_raw_parts<'a>(link: &'a summary::Link, out: &mut Vec<PageListParts<'a>>) {
@@ -95,6 +159,15 @@ impl Book {
     }
 }
 
+impl Page {
+    pub fn heirachy_element(&self) -> Link {
+        todo!()
+    }
+
+    pub fn url(&self) -> String {
+        todo!()
+    }
+}
 /// Fun helper type
 ///
 /// 1. Foo
@@ -210,7 +283,7 @@ mod pagetoc {
     //!    ],
     //!)
     //!```
-    #[derive(Debug, Clone)]
+    #[derive(Debug, Clone, Default)]
     pub struct PageToc(pub Vec<H2>);
 
     #[derive(Debug, Clone)]
@@ -229,4 +302,15 @@ mod pagetoc {
         pub this: Link,
         pub children: Vec<H3>,
     }
+}
+
+pub fn output_loc(input_loc: &Path, out_dir: &Path, base_dir: &Path) -> Result<PathBuf> {
+    let mut path = out_dir.join(input_loc.strip_prefix(base_dir)?);
+    if path.file_name() == Some(OsStr::new("README.md")) {
+        path.set_file_name("index.html")
+    } else {
+        path.set_extension("");
+        path.push("index.html");
+    }
+    Ok(path)
 }
