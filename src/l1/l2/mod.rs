20a7527d028c5ac5d6520ad320738eb5dce1dd56
Nixon Enraght-Moony <nixon.emoony@gmail.com>
2020-12-04 23:08:26 +0000

    Single file

--- src/l1/l2/mod.rs
+++
@@ -1,3 +0,0 @@
-pub mod l3;
-pub use l3::L4;
-
--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,13 @@
 
-pub mod l1;
+pub mod l1 {
+    pub mod l2 {
+        pub mod l3 {
+            pub struct L4;
+        }
+        pub use l3::L4;
+    }
+}
+
 
 fn main() {
 
