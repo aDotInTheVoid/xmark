a465dcace327acb7aea1e99b6a89834420e73042
Nixon Enraght-Moony <nixon.emoony@gmail.com>
2020-12-04 23:03:46 +0000

    Min

--- src/html_render/content/collect.rs
+++ src/html_render/content/collect.rs
@@ -1,11 +1,1 @@
-use std::path::PathBuf;
-
-
-
-#[derive(Debug, Clone, Default)]
-pub struct Dirs {
-    pub out_dir: PathBuf,
-    pub base_dir: PathBuf,
-    pub base_url: String,
-}
-
+pub struct Dirs;
