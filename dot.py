5b5469e6f945a7b1a312d4c46d5d771eec7da30d
Nixon Enraght-Moony <nixon.emoony@gmail.com>
2020-12-14 17:43:46 +0000

    Better dot

--- dot.py
+++ dot.py
@@ -3,11 +3,22 @@ import json
 import sys
 from graphviz import Digraph
 
-with open("./target/doc/xmark.json") as f:
-    inp = json.load(f)
 
-dot = Digraph("api", format="svg", engine="dot")
-root = inp["index"][inp["root"]]
+class Digraph2(Digraph):
+    def node2(self, name, label=None, _attributes=None, **attrs):
+        self.node(self._quote2(name), label, _attributes, **attrs)
+
+    def edge2(self, tail_name, head_name, label=None, _attributes=None, **attrs):
+        self.edge(
+            self._quote2(tail_name),
+            self._quote2(head_name),
+            label=None,
+            _attributes=None,
+            **attrs,
+        )
+
+    def _quote2(self, var):
+        return var.replace(":", "___")
 
 
 class ItemVisitor:
@@ -22,16 +33,27 @@ class ItemVisitor:
         return function(to_visit)
 
     def not_found(self, item):
- #       return
+        #       return
         raise Exception(f"No method `visit_{item['kind']}`")
 
-    def add_node(self, idx):
+    def add_node(self, idx, shape="ellipse"):
         name = self.inp["index"][idx]["name"]
-        path = "::".join(self.inp["paths"][idx]["path"])
-        self.dot.node(idx, label=path)
+        path = self.inp["paths"].get(idx)
+
+        path = "::".join(path["path"]) if path is not None else name
+        self.dot.node2(idx, label=path, shape=shape)
+
+    def extract_ty_id(self, ty):
+        kind = ty["kind"]
+        inner = ty["inner"]
+        if kind == "resolved_path":
+            # TODO: this returns Vec for Vec<Page>
+            idx = inner["id"]
+            if idx.split(":")[0] == "0":
+                return idx
 
     ###########################################################################
-    # Item Visitors                                                           #
+    # Core visitors                                                           #
     ###########################################################################
     def visit_module(self, module):
         for idx in module["inner"]["items"]:
@@ -41,23 +63,55 @@ class ItemVisitor:
         self.add_node(struct["id"])
         for idx in struct["inner"]["impls"]:
             self.visit(idx)
-    
-    def visit_impl(self, impl):
-        self.visit_module(impl)
 
     def visit_method(self, method):
         # And this is a pain, because for Vec<T>, (T, T),
         # &T, &[T], we want T, and that normalizarion is hard.
-        idx = method["id"]
-        decl = method["inner"]["decl"]
-        self.add_node(idx)
-        for arg in decl["inputs"]:
-     	    if isinstance
-        
+
+        fn_index = method["id"]
+        fn_decl = method["inner"]["decl"]
+
+        self.add_node(fn_index, shape="box")
+
+        for [_, in_ty] in fn_decl["inputs"]:
+            in_id = self.extract_ty_id(in_ty)
+            if in_id is not None:
+                self.dot.edge2(fn_index, in_id)
+
+        out_ty = fn_decl["output"]
+        if out_ty is not None:
+            out_id = self.extract_ty_id(out_ty)
+            if out_id is not None:
+                self.dot.edge2(fn_index, out_id)
+
+    ###########################################################################
+    # Passthoughts and ignores                                                #
+    ###########################################################################
+    def visit_enum(self, enum):
+        self.visit_struct(enum)
+
+    def visit_impl(self, impl):
+        self.visit_module(impl)
+
+    def visit_function(self, function):
+        self.visit_method(function)
+
+    def visit_import(self, _):
+        pass
+
+    def visit_typedef(self, _):
+        pass
+
+
+with open("./target/doc/xmark.json") as f:
+    inp = json.load(f)
+
+dot = Digraph2("api", format="svg", engine="dot")
+root = inp["index"][inp["root"]]
+
 
 v = ItemVisitor(inp, dot)
 v.visit(inp["root"])
 
-print(root)
 
 v.dot.render()
