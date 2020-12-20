9f52ed8933be84b410277d4e4359301cc8502684
Nixon Enraght-Moony <nixon.emoony@gmail.com>
2020-12-20 16:38:55 +0000

    Fresh promised land

--- src/main.rs
+++ src/main.rs
@@ -1,22 +1,3 @@
-// SPDX-License-Identifier: GPL-3.0-only
-pub(crate) mod cli;
-pub(crate) mod content;
-pub(crate) mod html_render;
-pub(crate) mod render;
-
-#[cfg(test)]
-mod test_utils;
-
-use eyre::{Result, WrapErr};
-
-fn main() -> Result<()> {
-    cli::init()?;
-
-    let args = cli::Args::parse()?;
-    let conf = cli::config::load(&args).context("Failed to load config")?;
-    let render = html_render::HTMLRender::new(&conf, &args)?;
-
-    render.render()?;
-
-    Ok(())
+fn main () {
+    xmark::main();
 }
